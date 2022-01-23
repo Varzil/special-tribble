5876d5d41801802a21f9c786c418ced8fe3b6629
Varzil <varzilthakkarvvv@gmail.com>
2022-01-23 15:02:21 +0530

    ADDING FASTREADER FILE

--- Code/Demo.java
+++ Code/Demo.java
@@ -3,5 +3,6 @@ package Code;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello This is my demo file");
+
     }
 }
--- Code/MINFD.java
+++ Code/MINFD.java
@@ -1,4 +1,10 @@
 package Code;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.StringTokenizer;
+
 /*
 Chef wants to make a purchase. For this, he needs X
  gold coins, but he has none at the moment.
@@ -25,8 +31,103 @@ and X as described in the statement.
  of which is Ai
 
 */
-public class MINFD {
-    public static void main(String[] args) {
+
+class FastReader
+{
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader()
+    {
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    }
+
+    String next()
+    {
+        while (st == null || !st.hasMoreElements())
+        {
+            try
+            {
+                st = new StringTokenizer(br.readLine());
+            }
+            catch (IOException  e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt()
+    {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong()
+    {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble()
+    {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine()
+    {
+        String str = "";
+        try
+        {
+            str = br.readLine();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
+
+
+
+class MINFD
+{
+    public static void main (String[] args)
+    {
+        try{
+            OutputStream outputStream =System.out;
+            PrintWriter out =new PrintWriter(outputStream);
+            FastReader input =new FastReader();
+            int t=input.nextInt();
+            while(t-->0){
+                int n=input.nextInt();
+                int x=input.nextInt();
+                int[] arr =new int[n];
+
+                int sum=0;
+                for(int i=0;i<n;i++)
+                    arr[i]=input.nextInt();
+
+                Arrays.sort(arr);
+                boolean flag=false;
+
+                for(int i=arr.length-1;i>=0;i--){
+
+                    sum+=arr[i];
+                    if(sum>=x)
+                    {
+                        flag=true;
+                        System.out.println(arr.length-i);
+                        break;
+                    }
+                }
+                if(!flag)
+                    System.out.println(-1);
+            }
+        }catch(Exception ignored){
+        }
 
     }
 }
+
