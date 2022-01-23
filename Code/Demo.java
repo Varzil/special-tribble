8760370f1ffd869095c695ee01e757875a6a453f
Varzil <varzilthakkarvvv@gmail.com>
2022-01-23 13:25:44 +0530

    ADDING FOLDER

---
+++ Code/Demo.java
@@ -0,0 +1,7 @@
+package Code;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello This is my demo file");
+    }
+}
---
+++ Code/MINFD.java
@@ -0,0 +1,32 @@
+package Code;
+/*
+Chef wants to make a purchase. For this, he needs X
+ gold coins, but he has none at the moment.
+
+Chef has N
+ fixed deposits, the ith
+ of which is worth Ai
+ coins. He wants to open the minimum number of these deposits so that he has at least X
+ coins.
+
+You have to tell Chef the minimum number of fixed deposits he must open in order to have X
+ coins, or say that this is impossible.
+
+INPUT FORMAT:
+==> The first line of input contains a single integer T
+, denoting the number of test cases. The description of T
+ test cases follows.
+
+==> The first line of each test case contains two space-separated integers — N
+and X as described in the statement.
+
+==> The second line of each test case contains N
+ space-separated integers — the ith
+ of which is Ai
+
+*/
+public class MINFD {
+    public static void main(String[] args) {
+
+    }
+}
